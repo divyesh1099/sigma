725873faaa9b06804a9f08f3451e929a5973f7a4
Divyesh <divyesh1099@gmail.com>
2020-12-20 13:45:48 +0530

    All apps jsut created

---
+++ crew5678/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class Crew5678Config(AppConfig):
+    name = 'crew5678'
---
+++ nautanki/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NautankiConfig(AppConfig):
+    name = 'nautanki'
